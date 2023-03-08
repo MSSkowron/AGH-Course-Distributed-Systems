30a236c60b2c51537383a890bf2360bbf44fe030
Mateusz Skowron <92554920+MSSkowron@users.noreply.github.com>
2023-03-08 23:19:58 +0100

    Delete skowron_mateusz_2 directory

--- skowron_mateusz_2/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/MSSkowron/skowron_mateusz_2
-
-go 1.19
-
-require github.com/gorilla/mux v1.8.0
--- skowron_mateusz_2/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/MSSkowron/skowron_mateusz_2/api"
-
-func main() {
-	s := api.New(":8080")
-	s.Run()
-}
